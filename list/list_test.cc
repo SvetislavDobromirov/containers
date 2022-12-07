8227758c9410e64dc9da04b23aeff3d34936f010
Your Name <you@example.com>
2022-12-08 02:19:41 +0300

    s

--- list/list_test.cc
+++ list/list_test.cc
@@ -2,13 +2,8 @@
 #include <vector>
 #include <iostream>
 int main () {
-   // std::list<int>  list_int = {1,2,3};
-    std::vector<int> obj;
-    for (int i = 0; i< 10; i++) obj.push_back(i);
-    std::cout << obj.capacity() << std::endl;
-    obj.clear();
-    std::cout << obj.capacity() << std::endl;
-    std::cout << obj.size() << std::endl;
+    std::list<int>  list_int;
+    std::cout << list_int.size() << std::endl;
 
     return 0;
 }
